6652bf577469f6955444b7c3948328d709a6b401
Henry Chan <henry@workos.com>
2020-04-07 19:38:12 -0400

    The basics

---
+++ config/workos.php
@@ -0,0 +1,11 @@
+<?php
+
+use WorkOS\Laravel\WorkOSServiceProvider;
+
+return [
+    // WorkOS API Key
+    'api_key' => env('WORKOS_API_KEY'),
+
+    // WorkOS Project ID
+    'project_id' => env('WORKOS_PROJECT_ID'),
+];
