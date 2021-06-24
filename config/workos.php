d1199c7503bad96ec190bab0bd7ff65106f6fa00
Blair Lunceford <blair@workos.com>
2021-06-24 11:41:12 -0600

    Remove project_id references

--- config/workos.php
+++ config/workos.php
@@ -9,9 +9,6 @@ return [
     // WorkOS Client ID
     "client_id" => env("WORKOS_CLIENT_ID"),
 
-    // WorkOS Project ID
-    "project_id" => env("WORKOS_PROJECT_ID"),
-
     // WorkOS base API URL
     "api_base_url" => null
 ];
