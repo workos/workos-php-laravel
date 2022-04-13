cc81890c5260d3e264c08639affa5250a9c04a2b
Adam Wolfman <47123183+awolfden@users.noreply.github.com>
2022-04-13 12:16:18 -0600

    Update Laravel SDK for PHP v1.9.0 (#31)
    * Update PHP SDK version requirement to 1.9.0
    
    * Update version of Laravel SDK to 1.5.0
    
    * Add new line at the end of composer.json
    
    Co-authored-by: Adam Wolfman <adamwolfman@Adams-MBP-3.domain>

--- lib/Version.php
+++ lib/Version.php
@@ -5,5 +5,5 @@ namespace WorkOS\Laravel;
 final class Version
 {
     public const SDK_IDENTIFIER = "WorkOS PHP Laravel";
-    public const SDK_VERSION = '1.4.0';
+    public const SDK_VERSION = '1.5.0';
 }
