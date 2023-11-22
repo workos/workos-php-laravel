d8ab9a1bb5293dea229c605a1725f962ce186ce2
Blair Lunceford <74257063+blairlunceford@users.noreply.github.com>
2023-11-22 16:18:29 -0700

    Bump version and workos-php to 4.0.0 (#50)

--- lib/Version.php
+++ lib/Version.php
@@ -5,5 +5,5 @@ namespace WorkOS\Laravel;
 final class Version
 {
     public const SDK_IDENTIFIER = "WorkOS PHP Laravel";
-    public const SDK_VERSION = '3.1.0';
+    public const SDK_VERSION = '4.0.0';
 }
