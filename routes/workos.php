661e62aad11c65173edd53637dbd544e2a735e99
Nick Nisi <nick.nisi@workos.com>
2025-02-10 12:20:07 -0600

    routes updates

--- routes/workos.php
+++ routes/workos.php
@@ -4,13 +4,17 @@ use App\Http\Controllers\WorkOS\WorkOSAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('web')->group(function () {
-	Route::get('/login/workos', function() {
+	Route::get('/auth/workos/login', function() {
 		return view('workos::login');
-	})->name('workos.login');
+	})->name('login');
 
 	Route::get('/auth/workos', [WorkOSAuthController::class, 'redirect'])->name('workos.redirect');
 
 	Route::get('/auth/workos/callback', [WorkOSAuthController::class, 'callback'])->name('workos.callback');
 
 	Route::get('/auth/workos/logout', [WorkOSAuthController::class, 'logout'])->name('workos.logout');
+
+	Route::get('/auth/workos/dashboard', function() {
+		return 'You are logged in!';
+	})->name('workos.dashboard')->middleware(('auth'));
 });
